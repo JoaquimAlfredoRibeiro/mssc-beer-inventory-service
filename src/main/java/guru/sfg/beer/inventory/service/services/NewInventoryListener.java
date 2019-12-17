0390d7a8fcf9a7addf782a110bf4710cc140b013
JoaquimAlfredoRibeiro <joaquimalfredoribeiro@gmail.com>
2019-12-17 15:16:33 +0000

    JMS Implementation

---
+++ src/main/java/guru/sfg/beer/inventory/service/services/NewInventoryListener.java
@@ -0,0 +1,30 @@
+package guru.sfg.beer.inventory.service.services;
+
+import guru.sfg.beer.inventory.service.config.JmsConfig;
+import guru.sfg.beer.inventory.service.domain.BeerInventory;
+import guru.sfg.beer.inventory.service.repositories.BeerInventoryRepository;
+import guru.sfg.common.events.NewInventoryEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.JmsListener;
+
+@Slf4j
+@RequiredArgsConstructor
+@Configuration
+public class NewInventoryListener {
+
+    private final BeerInventoryRepository beerInventoryRepository;
+
+    @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+    public void listen(NewInventoryEvent event){
+        log.debug("Got inventory: " + event.toString());
+
+        beerInventoryRepository.save(BeerInventory.builder()
+                                             .beerId(event.getBeerDTO().getId())
+                                             .upc(event.getBeerDTO().getUpc())
+                                             .quantityOnHand(event.getBeerDTO().getQuantityOnHand())
+                                             .build());
+    }
+
+}
